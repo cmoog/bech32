497e46349aa2dd5b8e5a53fa1200f4c6db991dab
Charlie Moog <moogcharlie@gmail.com>
2022-02-27 10:24:28 -0600

    add usage to readme

--- README.md
+++ README.md
@@ -8,3 +8,24 @@ A small command-line utility for encoding and decoding bech32 strings.
 ```
 cargo install --git https://github.com/cmoog/bech32
 ```
+
+## Usage
+
+```
+bech32 0.0.1
+
+USAGE:
+    bech32 [FLAGS] [OPTIONS] [data]
+
+FLAGS:
+    -d, --decode     Decode data. The human-readable prefix is discarded.
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+
+OPTIONS:
+    -p, --prefix <prefix>    The human-readable part of the encoded bech32 string.
+                             Example) "cm" is the prefix for cm1vfjkx6zlxve97cmvd90ksetvwq0h3xcp
+
+ARGS:
+    <data>    Data to encode or decode. Leave empty to use stdin.
+```
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,71 @@
 use anyhow::Result;
 use bech32::{self, FromBase32, ToBase32};
 use clap::{App, Arg};
-use std::io::{stdin, BufRead};
+use std::io::{stdin, stdout, Read, Write};
+use std::process;
+use std::str;
 
 fn main() {
     let matches = App::new("bech32")
         .version("0.0.1")
-        .arg(Arg::with_name("data").takes_value(true))
-        .arg(Arg::with_name("decode").short("d").long("decode"))
+        .arg(
+            Arg::with_name("data")
+                .takes_value(true)
+                .help("Data to encode or decode. Leave empty to use stdin."),
+        )
+        .arg(
+            Arg::with_name("decode")
+                .short("d")
+                .long("decode")
+                .help("Decode data. The human-readable prefix is discarded."),
+        )
         .arg(
             Arg::with_name("prefix")
                 .long("prefix")
                 .short("p")
-                .takes_value(true),
+                .takes_value(true)
+                .help(
+                    "The human-readable part of the encoded bech32 string.
+Example) \"cm\" is the prefix for cm1vfjkx6zlxve97cmvd90ksetvwq0h3xcp",
+                ),
         )
         .get_matches();
 
     match execute(matches) {
         Ok(_) => {}
-        Err(e) => eprintln!("{}", e),
+        Err(e) => {
+            eprintln!("{}", e);
+            process::exit(1);
+        }
     };
 }
 
 fn execute(matches: clap::ArgMatches) -> Result<()> {
-    if let Some(line) = matches.value_of("data") {
-        process_line(&matches, line)?;
-    } else {
-        let stdin = stdin();
-        let lines = stdin.lock().lines();
-        for line in lines {
-            process_line(&matches, line?.as_str())?;
+    match matches.value_of("data") {
+        None | Some("-") => {
+            let mut buf = Vec::new();
+            // TODO: read in chunks
+            let stdin = stdin();
+            stdin.lock().read_to_end(&mut buf)?;
+            process_line(&matches, buf)?;
         }
+        Some(data) => process_line(&matches, data.into())?,
     }
     Ok(())
 }
 
-fn process_line(matches: &clap::ArgMatches, line: &str) -> Result<()> {
+fn process_line(matches: &clap::ArgMatches, data: Vec<u8>) -> Result<()> {
     if matches.is_present("decode") {
-        let (_, data) = bech32::decode(line)?;
-        let base32 = Vec::<u8>::from_base32(&data)?;
-        let str = std::str::from_utf8(base32.as_slice())?;
-        println!("{}", str)
+        let mut raw = str::from_utf8(data.as_slice())?;
+
+        // trim trailing newline
+        // This is a hack and should probably be removed, but it
+        // ensures compatibility with simple usages of "echo", "cat", etc.
+        raw = raw.trim_end_matches("\n");
+
+        let (_, base32) = bech32::decode(raw)?;
+        let buf = Vec::<u8>::from_base32(&base32)?;
+        stdout().write_all(buf.as_slice())?;
     } else {
         let hrp = matches
             .value_of("prefix")
@@ -48,8 +73,8 @@ fn process_line(matches: &clap::ArgMatches, line: &str) -> Result<()> {
                 "--prefix required for encoding",
                 clap::ErrorKind::ArgumentConflict,
             ))?;
-        let s = bech32::encode(hrp, line.to_base32())?;
-        println!("encoded = {}", s);
+        let s = bech32::encode(hrp, data.to_base32())?;
+        println!("{}", s);
     }
     Ok(())
 }
