aabc52c536d71b62ff605964aebca717d27d847c
Charlie Moog <moogcharlie@gmail.com>
2022-02-27 13:28:45 -0600

    dev: add shell.nix

---
+++ shell.nix
@@ -0,0 +1,6 @@
+let
+  pkgs = import (fetchTarball ("https://github.com/NixOS/nixpkgs/archive/edaae6a28c25cc9dafcbb1156f29ee834b38aeb9.tar.gz")) { };
+in
+with pkgs; mkShell {
+  buildInputs = [ cargo rustc ];
+}
