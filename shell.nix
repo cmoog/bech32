c7bdf82139e34c0462ea1a707f8dd34d8ec88892
Charlie Moog <moogcharlie@gmail.com>
2022-02-27 22:55:19 -0600

    style

--- shell.nix
+++ shell.nix
@@ -2,5 +2,5 @@ let
   pkgs = import (fetchTarball ("https://github.com/NixOS/nixpkgs/archive/edaae6a28c25cc9dafcbb1156f29ee834b38aeb9.tar.gz")) { };
 in
 with pkgs; mkShell {
-  buildInputs = [ cargo rustc ];
+  packages = [ cargo rustc ];
 }
