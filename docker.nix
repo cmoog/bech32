1c82e637286e5c73f59223072f995b91f174e147
Charlie Moog <moogcharlie@gmail.com>
2022-05-03 15:57:29 -0500

    add docker tag

--- docker.nix
+++ docker.nix
@@ -1,7 +1,8 @@
-{ dockerTools, bech32 }:
+{ dockerTools, bech32, self }:
 
 dockerTools.buildImage {
   name = "bech32docker";
+  tag = self.shortRev or "dirty";
   config = {
     Entrypoint = [ "${bech32}/bin/bech32" ];
   };
--- flake.nix
+++ flake.nix
@@ -8,35 +8,33 @@
   };
 
   outputs = { self, nixpkgs, import-cargo, utils }:
-    utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          inherit (import-cargo.builders) importCargo;
-          crates = (importCargo { lockFile = ./Cargo.lock; inherit pkgs; }).cargoHome;
-          bech32 = with pkgs; stdenv.mkDerivation
-            {
-              name = "bech32";
-              src = self;
-              nativeBuildInputs = [ crates rustc cargo ];
-              buildPhase = "cargo build --release --offline";
-              installPhase = "install -Dm775 ./target/release/bech32 $out/bin/bech32";
-            };
-          bech32-docker = with pkgs; import ./docker.nix { inherit bech32; inherit (pkgs) dockerTools; };
-          bech32-app = {
-            type = "app";
-            program = "${bech32}/bin/bech32";
-          };
-        in
-        {
-          packages = { inherit bech32 bech32-docker; };
-          defaultPackage = bech32;
-          apps = { bech32 = bech32-app; };
-          defaultApp = bech32-app;
-          devShell = with pkgs; mkShell {
-            packages = [ cargo rustc rust-analyzer rustfmt ];
-            RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-          };
-        }
-      );
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (import-cargo.builders) importCargo;
+        crates = (importCargo { lockFile = ./Cargo.lock; inherit pkgs; }).cargoHome;
+        bech32 = with pkgs; stdenv.mkDerivation {
+          name = "bech32";
+          src = self;
+          nativeBuildInputs = [ crates rustc cargo ];
+          buildPhase = "cargo build --release --offline";
+          installPhase = "install -Dm775 ./target/release/bech32 $out/bin/bech32";
+        };
+        bech32-docker = with pkgs; import ./docker.nix { inherit bech32 self; inherit (pkgs) dockerTools; };
+        bech32-app = {
+          type = "app";
+          program = "${bech32}/bin/bech32";
+        };
+      in
+      {
+        packages = { inherit bech32 bech32-docker; };
+        defaultPackage = bech32;
+        apps = { bech32 = bech32-app; };
+        defaultApp = bech32-app;
+        devShell = with pkgs; mkShell {
+          packages = [ cargo rustc rust-analyzer rustfmt ];
+          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+        };
+      }
+    );
 }
