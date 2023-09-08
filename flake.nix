a6b003b5467a190ea8fe9096bf29a32f08824654
Charlie Moog <moogcharlie@gmail.com>
2023-09-08 14:08:26 -0500

    bump nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,34 +2,26 @@
   description = "A simple command-line utilty for encoding and decoding bech32 strings.";
 
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
-    naersk.url = github:nix-community/naersk;
-    flake-utils.url = github:numtide/flake-utils;
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, naersk, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs { inherit system; };
         naersk' = pkgs.callPackage naersk { };
         bech32 = naersk'.buildPackage {
           src = ./.;
         };
-        bech32-docker = pkgs.dockerTools.buildImage {
-          name = "bech32";
-          tag = self.shortRev or "dirty";
-          contents = [ bech32 ];
-          config.Entrypoint = [ "bech32" ];
-        };
-        bech32-app = {
-          type = "app";
-          program = "${bech32}/bin/bech32";
-        };
       in
       {
         formatter = pkgs.nixpkgs-fmt;
-        packages = { default = bech32; inherit bech32-docker; };
-        apps.default = bech32-app;
+        packages = { default = bech32; inherit bech32; };
         devShells.default = with pkgs; mkShell {
           packages = [ cargo rustc rust-analyzer rustfmt ];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
